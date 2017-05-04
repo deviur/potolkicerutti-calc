aeaf4769d2f848116bc8f511f5b2c7731c0f07d6
Deviur <deviur@yandex.ru>
2017-05-04 11:35:30 +0300

    added redux

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Alert from 'react-bootstrap/lib/Alert';
+
+import Itog from './Itog';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <div>
+            <Alert bsStyle="info">
+                <strong> ИТОГО: <Itog itog='50000'/></strong>
+            </Alert>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
     );
   }
 }
