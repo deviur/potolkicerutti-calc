fde8db57dfce09b573af2d804934387507a51ae7
Deviur <deviur@yandex.ru>
2017-05-06 18:59:01 +0300

    Промежуточные исправления в Tablo

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,21 @@ import Tablo from './Components/Tablo'
 class App extends Component {
 
   render() {
+    var itog = 50000;
+    var skidka_p = 0;
+    var skidka_r = 4000;
+    
+    if (skidka_p>0) {skidka_r = itog*skidka_p/100; itog -= skidka_r;}
+    if (skidka_r>0) {skidka_p = (skidka_r/itog)*100; itog -= skidka_r;}
+    /* Предполагается, что можно будет в компоненте Tablo кликнуть на itog, 
+    skidka_p или skidka_r, после чего можно будет ввести соответствущее
+    значение через Тег input. с событием onchange.
+      В этом случае в компонент Tablo можно будет передавать только itog, 
+    а расчет прибыли реализовать позднее*/    
+    
     console.log(Tablo);
     return (
-      <Tablo alert='' itog={45000} pribil={8000} skidka_p={10} skidka_r={5000}/>
+      <Tablo alert='' itog={itog} skidka_p={skidka_p} skidka_r={skidka_r}/>
     );
   }
 }
--- src/Components/Tablo.jsx
+++ src/Components/Tablo.jsx
@@ -12,34 +12,38 @@ class Tablo extends Component {
     pribil: PropTypes.number,
   };
   
-  toggleState() {
-    console.log(this.props.itog);
+  handleClick(event) {
+    console.log(event.target.id);
   }
 
   render() {
     
-    var itog =  (<strong>ИТОГО:{" "}{this.props.itog}</strong>);
-    var skidka_p = this.props.skidka_p;
+    var itog =  "ИТОГО: "+this.props.itog;
+    var skidka_p;
     var skidka_r = this.props.skidka_r;
-    var pribil = this.props.pribil;
+    var pribil;
     
+    if (this.props.skidka_p>0) skidka_p = 'Предоставлена скидка: ' + this.props.skidka_p + '%';
+    if (this.props.pribil>0) pribil = '(' + this.props.pribil + ')';
+      
     var alert = (<thead>
       <tr>
-        <th width="60%" >{this.props.alert}</th>
+        <th colSpan="4" width="60%" >{this.props.alert}</th>
       </tr>
     </thead>);
-    
     
     return (
       <Alert bsStyle={this.props.alert?'danger':'info'}>
         <Table condensed >
            {this.props.alert? alert:null}
           <tbody>
-            <tr>
-              <td id='itog' width='25%' onClick={this.toggleState.bind(this)}>{itog}</td>
-              <td id='skidka_p' width='25%' onClick={this.toggleState.bind(this)}>{(this.props.skidka_p)?'Скидка: ':''}{skidka_p} </td>
-              <td id='skidka_r' width='25%' onClick={this.toggleState.bind(this)}>{skidka_r}</td>
-              <td id='pribil' width='25%' onClick={this.toggleState.bind(this)}>{"("}{pribil}{")"}</td>
+            <tr style={{fontWeight: 'bolder'}}>
+              
+              <td id='itog' width='25%' onClick={this.handleClick.bind(this)}>{itog}</td>
+              <td id='skidka_p' width='25%' onClick={this.handleClick.bind(this)}>{skidka_p} </td>
+              <td id='skidka_r' width='25%' onClick={this.handleClick.bind(this)}>{skidka_r}</td>
+              <td id='pribil' width='25%' onClick={this.handleClick.bind(this)}>{pribil}</td>
+              
             </tr>
           </tbody>
         </Table>
